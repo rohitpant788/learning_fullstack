a1253fc859b0dc634c9b70dee7b83240825bdf6d
Rohit <rohit.pant.788@gmail.com>
2024-03-06 17:56:21 +0530

    Commit for 14.2 - Next js app with postgress integration prisma.

---
+++ 14.2/week-14-2/app/page.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+async function getUserData(){
+  const response = await axios.get("http://localhost:3000/api/user");
+  return response.data;
+}
+export default async function Home() {
+
+  //Adding some artificial delay to test loader
+  await new Promise((r)=>setTimeout(r,5000))
+
+
+  const userData = await getUserData();
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
+}
